e98107f437414bb089a1e446018366ac98868df5
NikitaPopovsky <popovsky.n@mail.ru>
2025-06-05 21:13:46 +0300

    Инициализация проекта

---
+++ src/test/java/com/github/NikitaPopovskiy/SeriesTracker/SeriesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.NikitaPopovskiy.SeriesTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
