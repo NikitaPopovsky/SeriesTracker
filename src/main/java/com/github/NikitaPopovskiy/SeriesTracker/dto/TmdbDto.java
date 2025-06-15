9b38ea7450c32d99de86c0f04b22eeed6df134dd
NikitaPopovsky <popovsky.n@mail.ru>
2025-06-16 00:41:41 +0300

    Реализация dto

--- src/main/java/com/github/NikitaPopovskiy/SeriesTracker/dto/TmdbDto.java
+++ src/main/java/com/github/NikitaPopovskiy/SeriesTracker/dto/TmdbDto.java
@@ -1,7 +1,23 @@
 package com.github.NikitaPopovskiy.SeriesTracker.dto;
 
+import com.fasterxml.jackson.annotation.*;
 import lombok.*;
+
+import java.time.*;
 
 @Data
 public class TmdbDto {
+    @JsonProperty ("id")
+    private int idTmdb;
+    private String name;
+    @JsonProperty ("original_name")
+    private String originalName;
+    @JsonProperty ("first_air_date")
+    private LocalDate firstAirDate;
+    private LocalDate overview;
+    @JsonProperty ("origin_country")
+    private LocalDate originCountry;
+    @JsonProperty ("poster_path")
+    private LocalDate posterPath;
+
 }
