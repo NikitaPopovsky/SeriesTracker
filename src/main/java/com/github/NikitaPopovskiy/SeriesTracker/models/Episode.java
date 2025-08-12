3dbfb59d6c62a80ede1d85c9f226dde192b0e462
NikitaPopovsky <popovsky.n@mail.ru>
2025-08-12 20:41:14 +0300

    - создание полей Episode

--- src/main/java/com/github/NikitaPopovskiy/SeriesTracker/models/Episode.java
+++ src/main/java/com/github/NikitaPopovskiy/SeriesTracker/models/Episode.java
@@ -1,4 +1,24 @@
 package com.github.NikitaPopovskiy.SeriesTracker.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.*;
+
+@Data
+@Entity
+@Table (name = "Episodes")
 public class Episode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "id_serial_tmdb")
+    private int idSerialTmdb;
+    @Column(name = "season_number")
+    private byte seasonNumber;
+    @Column(name = "episode_number")
+    private byte episodeNumber;
+    private String name;
+    @Column(name = "air_date")
+    private LocalDate airDate;
 }
